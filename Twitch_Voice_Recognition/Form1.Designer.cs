6212ac1f0c212ea1793cd7e303a8158a2ffb2506
Brian <shipofgenius@comcast.net>
2017-05-21 23:00:35 -0600

    Initial Commit

---
+++ Twitch_Voice_Recognition/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using KittenzTwitchLib;
+
+namespace Twitch_Voice_Recognition
+{
+    partial class frmVoiceRec
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(427, 214);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(60, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Listen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnListen_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(8, 8);
+            this.txtLog.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(511, 190);
+            this.txtLog.TabIndex = 1;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(427, 248);
+            this.btnStop.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(61, 27);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(9, 214);
+            this.btnConnect.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(64, 23);
+            this.btnConnect.TabIndex = 3;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.BtnConnect_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(9, 240);
+            this.btnDisconnect.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(72, 31);
+            this.btnDisconnect.TabIndex = 4;
+            this.btnDisconnect.Text = " Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            // 
+            // frmVoiceRec
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 352);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "frmVoiceRec";
+            this.Text = "Twitch Listen";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+          }
+
+        private void onTwitchLog(object sender, string message)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        #endregion
+
+        
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+    }
+}
+
---
+++ Twitch_Voice_Recognition/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using KittenzTwitchLib;
+
+namespace Twitch_Voice_Recognition
+{
+    public partial class frmVoiceRec : Form
+    {
+        //private server = 
+        SpeechRecognitionEngine vrEngine = new SpeechRecognitionEngine();
+        TwitchClient objClient = new TwitchClient("irc.chat.twitch.tv", 6667, "#trugamer_maniac", oauth, "trugamer_maniac");
+       
+        
+        public frmVoiceRec()
+        {
+            InitializeComponent();
+        }
+
+        private void btnListen_Click(object sender, EventArgs e)
+        {
+            vrEngine.RecognizeAsync();
+            objClient.OnTwitchConnected += connection_onTwitchConnect;
+            vrEngine.SpeechRecognized += vrEngine_SpeechRecognized;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Choices commands = new Choices();
+            commands.Add(new string[] { "chia seed", "cheese", "sugar", "hello" });
+            GrammarBuilder grammarBuild = new GrammarBuilder();
+            grammarBuild.Append(commands);
+            Grammar grammar = new Grammar(grammarBuild);
+            vrEngine.LoadGrammarAsync(grammar);
+            // SET AUDIO TO STREAM LATER 
+            vrEngine.SetInputToDefaultAudioDevice();
+            vrEngine.SetInputToAudioStream();
+           
+
+
+        }
+
+        void vrEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch (e.Result.Text)
+            {
+                case "hello":
+                    txtLog.Text += "\n Hello has been heard";
+                    break;
+                case "cheese":
+                    txtLog.Text += "\n cheese has been heard";
+                    break;
+                case "chia seed":
+                    txtLog.Text += "\n chia has been heard";
+                    break;
+                case "sugar":
+                    txtLog.Text += "\n sugar has been heard";
+                    break;
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            vrEngine.RecognizeAsyncStop();
+        }
+
+        private void BtnConnect_Click(object sender, EventArgs e)
+        {
+            objClient.Connect();
+        }
+           
+      void connection_onTwitchConnect(object sender)
+        {
+            MessageBox.Show("Connected!");
+            onTwitchLog(this, "log is working");
+        }
+
+        void connection_onTwitchLog(object sender, EventArgs e)
+        {
+            onTwitchLog(this, "log is working");
+        }
+
+
+    }
+}
